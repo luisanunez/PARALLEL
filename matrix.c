5a91b27072206f2a17a2c8cc8e68f8df34e87fdb
luisa <luisa.nunez@ucsp.edu.pe>
2015-10-05 23:11:22 -0500

    actualizacion

--- matrix.c
+++ matrix.c
@@ -1,122 +1,54 @@
+//gcc matrix.c -o matriz -lpthread
 
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#define ROWS 500
+#define COLUMNS 500
+#define N_THREADS 10
 
-int     thread_count;
-int     m, n;
-double* A;
-double* x;
-double* y;
+int mat1[ROWS][COLUMNS],mat2[ROWS][COLUMNS],res_mat[ROWS][COLUMNS];
 
-void Read_matrix(char* prompt, double A[], int m, int n);
-void Read_vector(char* prompt, double x[], int n);
-void Print_matrix(char* title, double A[], int m, int n);
-void Print_vector(char* title, double y[], int m);
-void *Pth_mat_vect(void* rank);
+void *mult_thread(void *t)
+{   
+    /*This function calculates 50 ROWS of the matrix*/
 
+    int starting_row;
+    starting_row = *((int *)t);
+    starting_row = 50 * starting_row;
 
-
-int main(int argc, char* argv[]) {
-   long       thread;
-   pthread_t* thread_handles;
-
-   thread_count = strtol(argv[1], NULL, 10);
-   thread_handles = malloc(thread_count*sizeof(pthread_t));
-
-   printf("Enter m and n\n");
-   scanf("%d%d", &m, &n);
-
-   A = malloc(m*n*sizeof(double));
-   x = malloc(n*sizeof(double));
-   y = malloc(m*sizeof(double));
-   
-   Read_matrix("Enter the matrix", A, m, n);
-   Print_matrix("We read", A, m, n);
-
-   Read_vector("Enter the vector", x, n);
-   Print_vector("We read", x, n);
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_create(&thread_handles[thread], NULL,
-         Pth_mat_vect, (void*) thread);
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_join(thread_handles[thread], NULL);
-
-   Print_vector("The product is", y, m);
-
-   free(A);
-   free(x);
-   free(y);
-   free(thread_handles);
-
-   return 0;
-}  
-
-
-
-
-
-void Read_matrix(char* prompt, double A[], int m, int n) {
-   int             i, j;
-
-   printf("%s\n", prompt);
-   for (i = 0; i < m; i++) 
-      for (j = 0; j < n; j++)
-         scanf("%lf", &A[i*n+j]);
-} 
-
-
-
-
-void Read_vector(char* prompt, double x[], int n) {
-   int   i;
-
-   printf("%s\n", prompt);
-   for (i = 0; i < n; i++) 
-      scanf("%lf", &x[i]);
-} 
-
-
-
-
-void *Pth_mat_vect(void* rank) {
-   long my_rank = (long) rank;
-   int i, j;
-   int local_m = m/thread_count; 
-   int my_first_row = my_rank*local_m;
-   int my_last_row = my_first_row + local_m - 1;
-
-   for (i = my_first_row; i <= my_last_row; i++) {
-      y[i] = 0.0;
-      for (j = 0; j < n; j++)
-          y[i] += A[i*(n+j)]*x[j];
-   }
-
-   return NULL;
+    int i,j,k;
+    for (i = starting_row;i<starting_row+50;i++)
+        for (j=0;j<COLUMNS;j++)
+            for (k=0;k<ROWS;k++)
+                res_mat[i][j] += (mat1[i][k] * mat2[k][j]);
+    return;
 }
 
-
-
-void Print_matrix( char* title, double A[], int m, int n) {
-   int   i, j;
-
-   printf("%s\n", title);
-   for (i = 0; i < m; i++) {
-      for (j = 0; j < n; j++)
-         printf("%4.1f ", A[i*n + j]);
-      printf("\n");
-   }
+void fill_matrix(int mat[ROWS][COLUMNS])
+{
+    int i,j;
+    for(i=0;i<ROWS;i++)
+        for(j=0;j<COLUMNS;j++)
+            mat[i][j] = 1;
 }
 
+int main()
+{
+    int n_threads = 10; //10 threads created bcos we have 500 rows and one thread calculates 50 rows
+    int j=0;
+    pthread_t p[n_threads];
 
-void Print_vector(char* title, double y[], int m) {
-   int   i;
+    fill_matrix(mat1);
+    fill_matrix(mat2);
+    for (j=0;j<10;j++)
+        pthread_create(&p[j],NULL,mult_thread,&j);
 
-   printf("%s\n", title);
-   for (i = 0; i < m; i++)
-      printf("%4.1f ", y[i]);
-   printf("\n");
-}  /* Print_vector */
+    for (j=0;j<10;j++)
+        pthread_join(p[j],NULL);
+
+    printf("%d\n",res_mat[1][1]);
+    return 0;
+}
