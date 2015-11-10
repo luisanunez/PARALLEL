bd48753b64d71937e144cf1aa7b92ad5cfda1f0e
luisa <luisa.nunez@ucsp.edu.pe>
2015-11-10 15:55:42 -0500

    tarea

---
+++ CH5/uno.c
@@ -0,0 +1,11 @@
+//gcc -fopenmp uno.c -o uno
+
+//Imprime un numero de la forma AAAAMM, que es el año y el mes de la especificación aprobada.
+
+
+#include <stdio.h>
+
+int main(void){
+	printf("%d\n", _OPENMP );
+}
+
